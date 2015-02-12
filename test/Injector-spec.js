0534b461f35e839ade966ea797ad43ed080e192d
petehurst <petehurst@users.noreply.github.com>
2015-02-12 21:38:53 +0000

    Manage dependencies

--- test/Injector-spec.js
+++ test/Injector-spec.js
@@ -1,13 +1,25 @@
-var Injector = require('../lib/Injector');
+var Injector = require("../lib/Injector");
 
-describe('Injector', function() {
+describe("Injector", function() {
 
   beforeEach(function() {
     this.injector = new Injector();
   });
 
-  it('can add a dependency', function(){
-    this.injector.register('foo', {});
+  it("can add a dependency", function(){
+    this.injector.register("foo", function(){});
   });
+
+  it("resolves a dependency", function(cb){
+    var dep = {};
+    this.injector.register("foo", function(){ return dep; });
+    this.injector.resolve("foo", function(err,result){
+      if (err) return cb(err);
+      result.should.equal(dep);
+      cb();
+    });
+  });
+
+  it("injects into a function");
 
 });
